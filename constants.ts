9b460a1fa6c6caff0e63e2457ea4e596e093116b
Chibueze <icukaegbu@gmail.com>
2024-09-16 08:44:03 +0100

    added constants and helper

---
+++ constants.ts
@@ -0,0 +1,360 @@
+export const colors = {
+  wine: '#6c006b',
+  black: '#000',
+  white: '#fff',
+  primary: '#862D7B',
+  activeLink: '#fff0e5',
+  activeBorder: '#ff6602',
+  grey: '#BFC5D2',
+  darkText: '#2E384D',
+  greyText: '#8798AD',
+  purple: '#6C006C',
+  orange: '#FF6600',
+  heading: '#7C7C7C',
+  tableHeading: '#BFC5D2',
+  red: '#DC3545',
+  blue: '#2E5BFF',
+  infoGrey: '#B0BAC9',
+  pending: '#17A2B8',
+  iconPurple: '#6F52ED',
+  lightPurple: 'rgba(113, 59, 219, 0.05)',
+  lightGreen: 'rgba(51, 214, 159, 0.07)',
+  iconGreen: '#21B8C7',
+  lightRed: 'rgba(255, 76, 97, 0.05)',
+  iconRed: '#FF4C61',
+  iconBlue: '#4CB8FF',
+  lightBlue: 'rgba(76, 184, 255, 0.07)',
+};
+
+export const daysOfWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+export const weeksOfYear = Array.from(
+  { length: 52 },
+  (_, i) => `Week ${i + 1}`,
+);
+
+export const monthsOfYear = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const staffTableHeaders = [
+  'Name',
+  'Email',
+  'Phone Number',
+  'Role',
+  'Status',
+  'Last Seen',
+];
+
+export const staffTableData = [
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Admin',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar1.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Sr. Customer Manager',
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar2.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar3.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar4.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar5.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar6.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar7.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar8.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar9.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+  {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '1234567890',
+    role: 'Staff',
+    status: 'Active',
+    lastSeen: '2021-01-01',
+    image: '/img/avatars/avatar10.png',
+    bio: "This theme adds additional client and channel information. It's useful for Admins when you need any ID of a user without searching for it.",
+    location: 'Lagos, Nigeria',
+    socials: [{ platform: 'Twitter', link: '@invisionapp' }],
+    position: 'Customer Manager',
+  },
+];
+
+export const complaintsTableHeaders = [
+  'Title',
+  'Description',
+  'Date',
+  'Resolved Date',
+  'Status',
+];
+
+export const studentsTableHeaders = [
+  'Name',
+  'Email',
+  'Age',
+  'Phone Number',
+  'Course',
+  'Last Seen',
+];
+
+export const feedbacksTableHeaders = ['Title', 'Date', 'Sender', 'Date'];
+
+export const complaintsTableData = [
+  {
+    title: 'Sign in issues',
+    description:
+      'I couldn’t sign in to check my account earlier Today because of a reason i don’t, I need help to sign in',
+    date: '06/12/2018',
+    resolvedDate: '07/05/2020',
+    status: 'Resolved',
+  },
+  {
+    title: 'Sign in issues',
+    description:
+      'I couldn’t sign in to check my account earlier Today because of a reason i don’t, I need help to sign in',
+    date: '06/12/2018',
+    resolvedDate: '---',
+    status: 'Pending',
+  },
+  {
+    title: 'Sign in issues',
+    description:
+      'I couldn’t sign in to check my account earlier Today because of a reason i don’t, I need help to sign in',
+    date: '06/12/2018',
+    resolvedDate: '07/05/2020',
+    status: 'Resolved',
+  },
+  {
+    title: 'Sign in issues',
+    description:
+      'I couldn’t sign in to check my account earlier Today because of a reason i don’t, I need help to sign in',
+    date: '06/12/2018',
+    resolvedDate: '---',
+    status: 'Pending',
+  },
+];
+export const studentsTableData = [
+  {
+    avatar: '/img/avatars/avatar1.png',
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 25,
+    phoneNumber: '1234567890',
+    course: 'Web Development',
+    lastSeen: '2021-01-01',
+  },
+  {
+    avatar: '/img/avatars/avatar1.png',
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 25,
+    phoneNumber: '1234567890',
+    course: 'Mobile App Development',
+    lastSeen: '2021-01-01',
+  },
+];
+export const feedbacksTableData = [
+  {
+    title: 'Sign In',
+    description:
+      'I think this software is really nice however it needs a little more understanding of the',
+    date: '06/12/2018',
+    sender: 'John Doe',
+  },
+  {
+    title: 'Sign In',
+    description:
+      'I think this software is really nice however it needs a little more understanding of the',
+    date: '06/12/2018',
+    sender: 'John Doe',
+  },
+  {
+    title: 'Sign In',
+    description:
+      'I think this software is really nice however it needs a little more understanding of the',
+    date: '06/12/2018',
+    sender: 'John Doe',
+  },
+];
+export const courses = [
+  {
+    title: 'Course 1',
+    numberOfStudents: 100,
+    averageMark: 50,
+    underPerformingStudents: 20,
+    underPerformingStudentsPercentage: '20',
+    finishedAssignment: '88%',
+  },
+  {
+    title: 'Course 2',
+    numberOfStudents: 50,
+    averageMark: 70,
+    underPerformingStudents: 30,
+    underPerformingStudentsPercentage: '30',
+    finishedAssignment: '70%',
+  },
+  {
+    title: 'Course 3',
+    numberOfStudents: 200,
+    averageMark: 55,
+    underPerformingStudents: 13,
+    underPerformingStudentsPercentage: '13',
+    finishedAssignment: '50%',
+  },
+];
+
+export const courseTableHeaders = [
+  'Course',
+  'Tutor',
+  'NO of students',
+  'Duration',
+  'Start Date',
+];
+
+export const courseTableData = [
+  {
+    course: 'Course 1',
+    tutor: 'John Doe',
+    numberOfStudents: 100,
+    duration: '2 months',
+    startDate: '2021-01-01',
+  },
+  {
+    course: 'Course 1',
+    tutor: 'John Doe',
+    numberOfStudents: 100,
+    duration: '2 months',
+    startDate: '2021-01-01',
+  },
+  {
+    course: 'Course 1',
+    tutor: 'John Doe',
+    numberOfStudents: 100,
+    duration: '2 months',
+    startDate: '2021-01-01',
+  },
+  {
+    course: 'Course 1',
+    tutor: 'John Doe',
+    numberOfStudents: 100,
+    duration: '2 months',
+    startDate: '2021-01-01',
+  },
+];
